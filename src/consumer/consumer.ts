1f72e8c678fb20b51cf8ebed9221c5475cb6490d
Parth Mangal <mangal.arpit.19@gmail.com>
2024-01-10 17:06:37 +0530

    Added Consumer Stop and Disconnect Function

--- src/consumer/consumer.ts
+++ src/consumer/consumer.ts
@@ -1,43 +1,52 @@
-import ConsumerConfig from "./consumer-config";
+import ConsumerConfig from './consumer-config';
 
-import {Kafka, Consumer as KafkaConsumer} from 'kafkajs'
-import {ConsumerRunConfig} from "./consumer-run-config";
+import { Kafka, Consumer as KafkaConsumer } from 'kafkajs';
+import { ConsumerRunConfig } from './consumer-run-config';
 
 export default class Consumer {
+	private config: ConsumerConfig;
+	private logger: any;
+	private consumer: KafkaConsumer;
+	private kafka: any;
+	constructor(config: ConsumerConfig, logger?: any) {
+		this.config = config;
+		this.logger = logger;
 
-  private config: ConsumerConfig;
-  private logger: any;
-  private consumer: KafkaConsumer;
-  private kafka: any
-  constructor(config: ConsumerConfig, logger?: any) {
-    this.config = config;
-    this.logger = logger;
+		const kafka = new Kafka({
+			clientId: this.config.groupId,
+			brokers: [...this.config.brokerList],
+		});
+		this.kafka = kafka;
+		const consumer = this.kafka.consumer({ groupId: this.config.groupId });
+		this.consumer = consumer;
+	}
 
-    const kafka = new Kafka({
-      clientId: this.config.groupId,
-      brokers: [...this.config.brokerList]
-    })
-    this.kafka = kafka;
-    const consumer = this.kafka.consumer({ groupId: this.config.groupId })
-    this.consumer = consumer;
-  }
+	public async connect(): Promise<void> {
+		await this.consumer.connect();
 
-  public async connect(): Promise<void> {
-    await this.consumer.connect();
+		// Subscribe to all topics
+		const promises: Array<Promise<any>> = [];
+		this.config.topics.forEach(topic => {
+			promises.push(
+				this.consumer.subscribe({
+					topic: topic,
+					fromBeginning: false,
+				}),
+			);
+		});
 
-    // Subscribe to all topics
-    const promises: Array<Promise<any>> = [];
-    this.config.topics.forEach(topic => {
-      promises.push(this.consumer.subscribe({
-        topic: topic,
-        fromBeginning: false
-      }));
-    });
+		await Promise.all(promises);
+	}
 
-    await Promise.all(promises);
-  }
+	public async consume(consumerRunConfig: ConsumerRunConfig): Promise<void> {
+		await this.consumer.run(consumerRunConfig);
+	}
 
-  public async consume(consumerRunConfig: ConsumerRunConfig): Promise<void> {
-    await this.consumer.run(consumerRunConfig);
-  }
+	public async disconnect(): Promise<void> {
+		await this.consumer.disconnect();
+	}
+
+	public async stop(): Promise<void> {
+		await this.consumer.stop();
+	}
 }
